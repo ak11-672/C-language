b01da6f4c3f16faa3e471b5ae0aaea6d5eb3def0
Mohammad Aseef Khan <155309894+ak11-672@users.noreply.github.com>
2025-01-18 11:26:14 +0500

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -1,5 +1,7 @@
 
-#include<stdio.h>                                                                            
+#include<stdio.h>        
+#include<stdlib.h>
+#include<string.h>
 void swappointers(int *p,int *q);
 void searchandcountch(char *str,char ch,int *arr);
 void uppercase(char *p);
